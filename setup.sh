e85810daa386965069ddc2cee77274748052676a
sinkthemall <tphat9a2@gmail.com>
2023-06-19 21:05:09 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
+#python dependencies
 sudo pip3 install pyautogui
 sudo apt-get install scrot
 sudo apt-get install python3-tk python3-dev
+
+
 git clone https://github.com/axboe/liburing.git
 cd ./liburing
-make
+./configure --cc=gcc --cxx=g++
+make -j$(nproc)
+sudo make install
